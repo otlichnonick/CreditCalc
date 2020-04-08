219f6fddc1798df7b6982f54059dbb1ce34a8291
Anton Agafonov <otlichnonick@yandex.ru>
2020-04-08 10:47:55 +0300

    Initial Commit

---
+++ FirstAppSwiftWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstAppSwiftWorld
+//
+//  Created by Антон on 08.04.2020.
+//  Copyright © 2020 Anton Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
